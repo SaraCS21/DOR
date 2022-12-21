53b101efd5354b58c5305252488081c67d20507d
SaraCS21 <saracs15.scssn@gmail.com>
2022-12-21 13:08:38 +0000

    Actividad web terminada

--- web_15-12-2022/index.js
+++ web_15-12-2022/index.js
@@ -1,11 +1,19 @@
-const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+const colorSwitch = document.querySelectorAll('.switch input[type="checkbox"]');
+const arrayColorSwitch = [...colorSwitch];
+
+const logo = document.querySelector("#logo");
+const img = document.querySelector("#img1");
 
 function cambiaTema(ev){
     if(ev.target.checked){
         document.documentElement.setAttribute('tema', 'light');
+        logo.src = "./img/logo_dark.png";
+        img.src = "./img/img1_dark.png";
     } else {
         document.documentElement.setAttribute('tema', 'dark');
+        logo.src = "./img/logo.png";
+        img.src = "./img/img1.png";
     }
 }
 
-colorSwitch.addEventListener('change', cambiaTema);
+arrayColorSwitch.forEach(element => element.addEventListener('change', cambiaTema));
